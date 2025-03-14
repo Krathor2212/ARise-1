63a79224bb3d22309fd81622cdde11966fdad879
GOPI3012 <163634824+GOPI3012@users.noreply.github.com>
2025-03-14 10:08:25 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import connectDB from './config/db.js';
 import authRoutes from './routes/authRoutes.js';
 import dotenv from 'dotenv';
+import topicsRoutes from './routes/topicsRoutes.js';
+import subtopicsRoutes from './routes/subtopicsRoutes.js';
 
 dotenv.config();
 
@@ -15,6 +17,8 @@ app.use(express.json());
 
 // Routes
 app.use('/auth', authRoutes);
+app.use('/api/topics', topicsRoutes);
+app.use('/api/subtopics', subtopicsRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
