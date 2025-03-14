7c86eb182b6255d049501513bb3686a84a3d064e
SreeRam-TR <161915125+SreeRam-TR@users.noreply.github.com>
2025-03-14 12:32:47 +0530

    Update authRoutes.js for get user profile details and also include getAll Users to get the list of users

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import { signup, login } from '../controllers/authController.js';
+import { getUserProfile, getAllUsers } from '../controllers/userController.js';
 
 const router = express.Router();
 
 router.post('/signup', signup);
 router.post('/login', login);
+router.get('/profile',getUserProfile);
+router.get('/users',getAllUsers);
 
 export default router;
