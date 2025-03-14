d6fce90469c9350aa9f112c8f3345176b1cf44b1
GOPI3012 <163634824+GOPI3012@users.noreply.github.com>
2025-03-14 19:52:55 +0530

    Update Topic.js

--- Backend/models/Topic.js
+++ Backend/models/Topic.js
@@ -1,20 +1,18 @@
 import mongoose from 'mongoose';
 
-// Define the schema for subtopics (including nested subtopics)
 const subtopicSchema = new mongoose.Schema({
     id: String,
     title: { type: String, required: true },
-    subtopics: { type: [this], default: [] } // Recursive structure for nested subtopics
+    subtopics: { type: [this], default: [] } 
 });
 
-// Define the schema for topics
+
 const topicSchema = new mongoose.Schema({
     unit: Number, // Unit number
-    title: { type: String, required: true }, // Title of the topic
-    subtopics: { type: [subtopicSchema], default: [] } // Array of subtopics
+    title: { type: String, required: true }, 
+    subtopics: { type: [subtopicSchema], default: [] }
 });
 
-// Define the model with the correct collection name "chapter_demo"
 const Topic = mongoose.model('Topic', topicSchema, 'chapter_demo');
 
 export default Topic;
